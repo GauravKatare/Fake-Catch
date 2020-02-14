1f8faeeecb88a98ca5af6ba4c3d2a3f911f48769
KUNAL ANAND <anandkunal241@gmail.com>
2020-02-15 02:12:13 +0530

    mixtwoaudio updated

--- mixtwoaudio.py
+++ mixtwoaudio.py
@@ -1,31 +1,29 @@
 from rev_ai import apiclient, JobStatus
 from pydub import AudioSegment
 
-def fun(filename):
+def fun(file_path,sample):
     access_token = '02UMSGJJclAaCjsV2JhZn2cVfRiFTo4NBlQtKycPT6k5IxaGNumOSU7a33Bc-uaGXjnXKbo8y1IaVjMHOchz6ua68Lb7U'
+
+    # Create client with your access token
     client = apiclient.RevAiAPIClient(access_token)
-    file_job = client.submit_job_local_file(filename, metadata="This_is_some_job_metadata", callback_url="", skip_diarization=False)
+
+    file_job = client.submit_job_local_file(filename= file_path, metadata="This_is_some_job_metadata", callback_url="", skip_diarization=False)
+
     job_id = file_job.id
+
     while file_job.status == JobStatus.IN_PROGRESS :
         file_job=client.get_job_details(job_id)
-    sample = "crush"
-
-    captions = client.get_captions(job_id)
-    print(captions)
-    #print('ok')
-
     mylist = []
     file = client.get_transcript_json(job_id)
 
     for i in file['monologues']:
         for j in i['elements']:
-            if j["type"] == "text" and j["value"] == sample:
+            if j['type'] == "text" and j["value"] == sample:
                 new_tuple = (j["ts"], j["end_ts"])
                 mylist.append(new_tuple)
-
     return mylist
 
-mylist=fun("/home/gaurav/Documents/raj.wav")
+mylist=fun("/home/gaurav/Documents/raj.wav", "Enter the text to be replaced")
 song=AudioSegment.from_mp3("/home/gaurav/Documents/raj.wav")
 song1=AudioSegment.from_mp3("/home/gaurav/Downloads/downloads.wav")
 print(mylist)
